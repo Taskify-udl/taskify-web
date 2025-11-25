ee13ac0b5ea2ba3a1ab0e1c3ab3a75d7bf82414d
Sergi Vila Monguia <47522460+Troter2@users.noreply.github.com>
2025-11-25 18:52:37 +0100

    update

--- taskify_app/models/__init__.py
+++ taskify_app/models/__init__.py
@@ -4,7 +4,6 @@ from .service_category import ServiceCategory
 from .contract import Contract
 from .review import Review
 from .favorite import Favorite
-from .user_profile import UserProfile
 from .notification import Notification
 from .user import CustomUser
 from .email_verification import EmailVerification
