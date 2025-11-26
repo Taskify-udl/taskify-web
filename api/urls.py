ac5b745f9dec68f0b13863eea123df7509a692bd
Oriol Soldevila <oriolsoldevila09@gmail.com>
2025-11-26 18:51:04 +0100

    feat: get the verification code

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import review_views, contract_views, category_views, service_views, favorite_views, auth_views, user_views
+from .views import review_views, contract_views, category_views, service_views, favorite_views, auth_views, user_views, email_verification_views
 from taskify_app import views
 
 urlpatterns = [
@@ -18,4 +18,5 @@ urlpatterns = [
     path('favorite', favorite_views.favorites),  # GET y POST
     path('favorite/<int:pk>', favorite_views.favorite_detail),  # DELETE
     path('chat/<int:conversation_id>/new-messages/', views.get_new_messages, name='get_new_messages'),
+    path('verification-code', email_verification_views.get_verification_code),
 ]
